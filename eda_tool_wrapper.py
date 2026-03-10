1c153b90549a2b2c9057bd43589b1b63176a1bb9
matthewnichols8 <mattnichols192@icloud.com>
2026-03-10 12:12:03 -0400

    Added Enum Descriptions

--- eda_tool_wrapper.py
+++ eda_tool_wrapper.py
@@ -4,9 +4,11 @@ from enum import Enum, auto
 @dataclass
 class ToolStatus(Enum):
     """ Displays the Tool's Current Status """
-    NOT_RUN = auto()
-    RUNNING = auto()
-    SUCCESS = auto()
-    WARNING = auto()
-    ERROR   = auto()
+    NOT_RUN = auto() # tool not running
+    RUNNING = auto() # subprocess is active
+    SUCCESS = auto() # returncode == 0, no warnings
+    WARNING = auto() # returncode == 0, stdout contains warning
+    ERROR   = auto() # returncode != 0
+
+
 
